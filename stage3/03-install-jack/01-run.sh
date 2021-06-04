35e94b11d7f243b75c0d29a519d46cd6ac92ea46
kdoren <kevin@doren.org>
2021-06-03 18:38:08 -0700

    use updated jack service file instead of from package

--- stage3/03-install-jack/01-run.sh
+++ stage3/03-install-jack/01-run.sh
@@ -1,3 +1,5 @@
 
 mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/aj-snapshot
 cp files/*.xml ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/aj-snapshot
+
+cp files/jack.service ${ROOTFS_DIR}/usr/lib/systemd/system/
