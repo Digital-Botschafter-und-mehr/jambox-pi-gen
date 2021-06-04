f8ff137935146b81ff1f54f5349e6758b74f1ee2
kdoren <kevin@doren.org>
2021-06-03 17:17:28 -0700

    auto configure dtoverlay for hifiberry dac+ adc pro

--- stage3/06-install-jamulus/files/jambox_start.sh
+++ stage3/06-install-jamulus/files/jambox_start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# On first boot, if hifiberry DAC+ ADC Pro is visible in /proc/device-tree, enable in /boot/config.txt and reboot
+if [ -f /proc/device-tree/hat/product ]; then
+  if grep -q 'DAC+ ADC Pro' /proc/device-tree/hat/product; then
+    if grep -q '^#force_eeprom_read=0' /boot/config.txt; then
+      if grep -q '^#dtoverlay=hifiberry-dacplusadcpro' /boot/config.txt; then
+        sudo sed -i 's/^#force_eeprom_read=0/force_eeprom_read=0/' /boot/config.txt
+        sudo sed -i 's/^#dtoverlay=hifiberry-dacplusadcpro/dtoverlay=hifiberry-dacplusadcpro/' /boot/config.txt
+        sudo reboot
+      fi
+    fi
+  fi
+fi
+
 if [ -f ~/.config/Jamulus/jamulus_start.conf ]; then
   source ~/.config/Jamulus/jamulus_start.conf
   if [[ "$JAMULUS_AUTOSTART" == '1' ]]; then
@@ -15,3 +29,10 @@ if [ -f ~/.config/sonobus_start.conf ]; then
   fi
 fi
 
+if [ -f ~/.config/JammerNetz/jammernetz_start.conf ]; then
+  source ~/.config/JammerNetz/jammernetz_start.conf
+  if [[ "$JAMMERNETZ_AUTOSTART" == '1' ]]; then
+   jammernetz_start.sh
+   exit 0
+  fi
+fi
