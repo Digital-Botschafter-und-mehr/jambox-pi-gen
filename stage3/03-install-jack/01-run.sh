f5efecd1b895741f9119f0d3345b04bc3146d523
kdoren <kevin@doren.org>
2021-07-20 04:40:08 -0700

    remove unused source

--- stage3/03-install-jack/01-run.sh
+++ stage3/03-install-jack/01-run.sh
@@ -3,3 +3,13 @@ mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/aj-snapshot
 cp files/*.xml ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/aj-snapshot
 
 cp files/jack.service ${ROOTFS_DIR}/usr/lib/systemd/system/
+
+on_chroot << EOF
+	adduser --no-create-home --system --group jack
+	adduser jack audio --quiet
+	adduser pi jack --quiet
+	adduser root jack --quiet
+	echo "JACK_PROMISCUOUS_SERVER=jack" >> /etc/environment
+	# systemctl enable jack
+EOF
+
